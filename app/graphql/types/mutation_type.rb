0d85ebd74cf8b42169598b7945b54d0fffb0af59
TAKU1022 <nase49946@gmail.com>
2026-01-17 22:08:53 +0900

    feat: update_bookミューテーションの作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::ObjectTypes::BaseObject
     field :create_book, mutation: Mutations::CreateBook
+    field :update_book, mutation: Mutations::UpdateBook
   end
 end
