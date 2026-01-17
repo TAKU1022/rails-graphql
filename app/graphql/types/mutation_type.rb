b49f857ab14232e8b103f15340f3054fd16f39f4
TAKU1022 <nase49946@gmail.com>
2026-01-17 22:35:45 +0900

    feat: delete_bookミューテーションの作成

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
   class MutationType < Types::ObjectTypes::BaseObject
     field :create_book, mutation: Mutations::CreateBook
     field :update_book, mutation: Mutations::UpdateBook
+    field :delete_book, mutation: Mutations::DeleteBook
   end
 end
